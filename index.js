3577a5d630ee6f902fe2e284675f9e53e460c246
artyNL <arty@duck.com>
2022-03-22 11:30:03 +0100

    dsa

--- index.js
+++ index.js
@@ -34,10 +34,13 @@ app.get("", (req, res) => {
     res.render("index");
 })
 
+app.get("/login", (req,res) => {
+    res.render("login");
+})
+
 app.get("/dashboard", (req, res) => {
     res.render("dashboard", {data: "gebruiker"});
 })
-
 
 app.listen(3000, () => {
     console.log("Backend server is aan het lopen!");
