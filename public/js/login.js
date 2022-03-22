9ad886728e452a3c9dac74b596d518fa9f05aece
qwertycho <92020588+qwertycho@users.noreply.github.com>
2022-03-22 11:02:33 +0100

    animatie

--- public/js/login.js
+++ public/js/login.js
@@ -3,10 +3,18 @@ localStorage.setItem("account", "login")
 document.getElementById("form").innerHTML = `
 <div class="slide" id="slide">
 <div class="slideBlock" id="slideBlock">Log In</div>
-<p class="blockTekst">Sign Up</p>
+<div class="slideBlock2" id="slideBlock2">signUp</div>
 </div>
 `
 
+document.getElementById("form").innerHTML += `
+        <form id="login" class="login">
+        <label>Naam</label>
+        <input type="text" placeholder="Naam">
+        <label>Wachtwoord</label>
+        <input type="password" placeholder="Wachtwoord">
+        </form>
+        `
 
 function wissel(){
 console.log("lgtm")
@@ -25,51 +33,48 @@ console.log("lgtm")
 function load(){
     try{
     document.getElementById("login").innerHTML = ""
+    console.log(localStorage.getItem("account"))
     }catch(err){
 
     }
 
 switch(localStorage.getItem("account")){
     case "login":
-        document.getElementById("form").innerHTML += `
-        <form class="login">
+        console.log("login")
+        document.getElementById("login").innerHTML += `
         <label>Naam</label>
         <input type="text" placeholder="Naam">
         <label>Wachtwoord</label>
         <input type="password" placeholder="Wachtwoord">
         <input class="remember" id="remember" type="checkbox"><label class="remember" id="remember">Onthoud mij</label>
-        </form>
         `
-        document.getElementById("slideBlock").style.animation = "login 1s"
-        // document.getElementById("slideBlock").style.animationDuration = "1s"
-        // document.getElementById("slideBlock").style.float = "left"
+        document.getElementById("slideBlock").style.animation = "right 1s"
+        document.getElementById("slideBlock").style.marginLeft = "-5%"
+        document.getElementById("slideBlock").style.backgroundColor = "var(--buttonActive)"
+        document.getElementById("slideBlock2").style.animation = "left 1s"
+        document.getElementById("slideBlock2").style.marginLeft = "0%"
+        document.getElementById("slideBlock2").style.backgroundColor = "var(--buttonNope)"
+
         break;
     case "signup":
-        document.getElementById("form").innerHTML += `
-        <form class="login">
+        console.log("signup")
+        document.getElementById("login").innerHTML += `
         <label>Naam</label>
         <input type="text" placeholder="Naam">
         <label>E-mail</label>
         <input type="email" placeholder="E-mail">
         <label>Wachtwoord</label>
         <input type="password" placeholder="Wachtwoord">
-        </form>
         `
-        document.getElementById("slideBlock").style.float = "right"
+        document.getElementById("slideBlock2").style.animation = "right 1s"
+        document.getElementById("slideBlock2").style.marginLeft = "-5%"
+        document.getElementById("slideBlock2").style.backgroundColor = "var(--buttonActive)"
+        document.getElementById("slideBlock").style.animation = "left 1s"
+        document.getElementById("slideBlock").style.marginLeft = "0%"
+        document.getElementById("slideBlock").style.backgroundColor = "var(--buttonNope)"
         break;
-    default:
-        document.getElementById("form").innerHTML += `
-        <form class="login">
-        <label>Naam</label>
-        <input type="text" placeholder="Naam">
-        <label>Wachtwoord</label>
-        <input type="password" placeholder="Wachtwoord">
-        </form>
-        `
-        document.getElementById("slideBlock").style.animation = "signup 1s"
-        // document.getElementById("slideBlock").style.float = "left"
-    }   
+}
     document.getElementById("slide").addEventListener("click", wissel)
 }
-load()
+document.getElementById("slide").addEventListener("click", wissel)
 
