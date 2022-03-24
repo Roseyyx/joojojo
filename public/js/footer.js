80092a358b2c93d244006c8791162911e19de38e
qwertycho <tychovopstal@gmail.com>
2022-03-24 11:29:54 +0100

    index style

---
+++ public/js/footer.js
@@ -0,0 +1,17 @@
+document.getElementById("footer").innerHTML = `
+<ul id="links">
+                <li><a href="" class="footer-link footerKnop">Home</a></li>
+                <li><a href="" class="footer-link footerKnop">Nieuws</a></li>
+                <li><a href="" class="footer-link footerKnop">Tickets</a></li>
+                <li><a href="" class="footer-login footerKnop">Login</a></li>
+                <li><a href="" class="footer-register footerKnop">Register</a></li>
+            </ul>
+        <div id="line"></div>
+        <div id="contact">
+            <ul id="rechts">
+                <li><a href="" class="footer-link footerKnop">Contact</a></li>
+                <li><a href="" class="footer-link footerKnop">Feesboek</a></li>
+                <li><a href="" class="footer-link footerKnop">Twotter</a></li>
+            </ul>
+        </div>
+`
---
+++ public/js/nav.js
@@ -0,0 +1,14 @@
+document.getElementById("nav").innerHTML = `
+<div class="nav-wrapper">
+            <a href="" class="nav-link homeButton">Home</a>
+            <div class="linkys">
+                <li><a href="" class="nav-link">Nieuws</a></li>
+                <li><a href="" class="nav-link">|</a></li>
+                <li><a href="" class="nav-link">Tickets</a></li>
+            </div>
+            <div class="nav-btn-wrapper">
+                <a href=""><button class="nav-link">Login</button></a>
+                <a href=""><button class="nav-link">Register</button></a>
+            </div>
+        </div>
+`
