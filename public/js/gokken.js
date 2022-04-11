bb60d0c08179d8d3300426ad169e3a431f021cb7
qwertycho <92020588+qwertycho@users.noreply.github.com>
2022-04-11 09:15:27 +0200

    gokken

--- public/js/gokken.js
+++ public/js/gokken.js
@@ -1,10 +1,24 @@
 const coureurs = ["Verstappe", "Leclerc", "Hamilton", "Russell"];
+const uitslag = {
+    bekend: false,
+    winnaar: "",
+};
 
-document.getElementById("submit").addEventListener("click", function(event) {
-    // prevent default
-    event.preventDefault();
-    let win = randomWeighted([0.5, 0.2, 0.2, 0.2]);
-    alert(`Je hebt ${win ? "gewonnen" : "verloren"}!`);
+document.getElementById("submit").addEventListener("click", function (event) {
+  event.preventDefault();
+  // sla het antwoord op
+  // haal het bedrag van het account
+  let bedrag = document.getElementById("bedrag").value;
+  if (bedrag > 0) {
+    update();
+    if (uitslag.bekend) {
+      alert(`Je hebt ${result() ? "gewonnen" : "verloren"}!`);
+    } else {
+      update();
+    }
+  } else {
+    alert("Je hebt geen geld!");
+  }
 });
 
 // random weighted number generator
@@ -23,5 +37,38 @@ function randomWeighted(weights) {
     }
 }
 
+function result(){
+    return win = randomWeighted([0.5, 0.2, 0.2, 0.2]);
+}
+
+// timer functie
+
+let oldDate = new Date();
+
+function sleep(milliseconds) {
+  return new Promise((resolve) => setTimeout(resolve, milliseconds));
+}
+async function update() {
+  await sleep(250);
+
+    const date = new Date();
+    if (oldDate != date) {
+      const minutes = date.getMinutes();
+      if (minutes == 30 || minutes == 0) {
+          update();
+          uitslag.bekend = true;
+      } else {
+          if(minutes < 30){
+        document.getElementById("countdown").innerHTML =
+          "De volgende uitslag word bekend over: " + (30 - minutes) + " minuten";
+          } else {
+                document.getElementById("countdown").innerHTML = "De volgende uitslag word bekend over: " + (60 - minutes) + " minuten";
+            }
+          oldDate = date;
+          uitslag.bekend = false;
+          update();
+      }
+    }
+}
 
 
